4e0a5de7ef1882a4e73d50aa71a326150e1aa122
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-11-13 11:50:06 +0300

    fixes type err

--- index.ts
+++ index.ts
@@ -16,10 +16,10 @@ bot.onText(/\/echo (.+)/, (msg: Message, match: RegExpExecArray | null) => {
   // of the message
 
   const chatId = msg.chat.id;
-  const resp = match && match[1]; // the captured "whatever"
+  const resp = match![1]; // the captured "whatever"
 
   // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp as string);
+  bot.sendMessage(chatId, resp);
 });
 
 // Listen for any kind of message. There are different kinds of
