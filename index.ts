1b881194c431f6fb7dfde5325973cf47b867dc81
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-11-21 16:33:29 +0300

    minor cleanup

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-import {config} from 'dotenv';
-import * as TelegramBot from "node-telegram-bot-api";
-import { Message } from 'node-telegram-bot-api';
+import { config } from 'dotenv';
+import * as TelegramBot from 'node-telegram-bot-api';
 
 config();
 // replace the value below with the Telegram token you receive from @BotFather
@@ -10,7 +9,7 @@ const token = process.env.TELEGRAM_BOT_TOKEN as string;
 const bot = new TelegramBot(token, { polling: true });
 
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg: Message, match: RegExpExecArray | null) => {
+bot.onText(/\/echo (.+)/, (msg: TelegramBot.Message, match: RegExpExecArray | null) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
@@ -24,7 +23,7 @@ bot.onText(/\/echo (.+)/, (msg: Message, match: RegExpExecArray | null) => {
 
 // Listen for any kind of message. There are different kinds of
 // messages.
-bot.on('message', (msg: Message) => {
+bot.on('message', (msg: TelegramBot.Message) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
