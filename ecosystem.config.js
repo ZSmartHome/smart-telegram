df3e2391ac0bbbca959939a9ed16390d487452a4
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-12-18 02:23:08 +0300

    added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+
+    // First application
+    {
+      name: 'Smart TelegramBot',
+      script: 'build/index.js'
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    dev: {
+      user: 'pi',
+      host: '192.168.2.7',
+      ref: 'origin/master',
+      repo: 'https://github.com/zeckson/smart-telegram.git',
+      path: '/home/pi/smartbot',
+      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env dev'
+    }
+  }
+};
