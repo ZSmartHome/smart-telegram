944ba64c58fe9b580ec5707ee8a6a04c2d447768
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-12-21 05:06:18 +0300

    fix message

--- script/me.ts
+++ script/me.ts
@@ -10,8 +10,8 @@ export const setup = (bot: TelegramBot, manage: Manage) => {
       message = `You are anonymous =(`;
     } else {
       message = `Here what I know about you:
-${me.username} ${me.first_name} ${me.last_name} \
-(${manage.isAuthorized(me) ? `` : `not`} authorized)`;
+@${me.username} ${me.first_name} ${me.last_name} \
+(${manage.isAuthorized(me) ? `` : `not `}authorized)`;
     }
     bot.sendMessage(msg.chat.id, message, {parse_mode: `Markdown`});
   });
