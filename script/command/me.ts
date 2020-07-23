ce7427793aa16b8ed9c6433373a111e600839e7d
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-07-24 00:32:25 +0300

    print root

--- script/command/me.ts
+++ script/command/me.ts
@@ -8,13 +8,15 @@ export default class MeCommand extends Command {
   public authRequired = false;
 
   public handleMessage(msg: TelegramBot.Message): void {
+    const root = this.manage.root;
+
     const me = msg.from;
     let message;
     if (!me) {
       message = `You are anonymous =(`;
     } else {
       message = `Here what I know about you:
-id:${me.id} ${me.first_name || ``} ${me.last_name || ``}${me.username ? ` @${me.username}` : ``} \
+id:${me.id}${root === me.id ? `(root)` : ``} ${me.first_name || ``} ${me.last_name || ``}${me.username ? ` @${me.username}` : ``} \
 (${this.manage.isAuthorized(me) ? `` : `not `}authorized)`;
     }
     this.bot.sendMessage(msg.chat.id, message, {parse_mode: `Markdown`});
